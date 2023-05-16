7ec34ca66b359a4249fab76f25b8e64b81e5fe17
kataviz <kataviz@users.noreply.github.com>
2023-05-15 20:43:29 -0400

    update app in docs/

--- ui/app/src/components/TrendsTimeline.tsx
+++ ui/app/src/components/TrendsTimeline.tsx
@@ -1,3 +1,4 @@
+import { randomNormal, randomUniform, range } from 'd3'
 import React from 'react'
 
 type Props = {
@@ -12,8 +13,28 @@ export default function TrendsTimeline({genes}: Props) {
 }
 
 function randomDataGenerator(glist:string[]){
-  let randomData = [];
+  let randomData:any[] = [];
+  const startDate = new Date('2018-01-01');
+  const endDate = new Date();
+  const millisecondsPerDay: number = 1000 * 60 * 60 * 24;
+
+  const days = Math.floor((endDate.getTime() - startDate.getTime()) / millisecondsPerDay);
+  console.log(glist); 
+
+
+  let seed = randomUniform(10, 100)()
   glist.forEach((g)=>{
-    
+    let date = new Date('2018-01-01');
+
+    range(days-1).forEach((day)=>{
+      randomData.push({
+        gene: g,
+        date: date.toDateString(),
+        cites: randomNormal(seed, seed*0.1)()
+      })
+      date.setDate(date.getDate() + 1);
+    })
   })
+
+  console.log(randomData)
 }
